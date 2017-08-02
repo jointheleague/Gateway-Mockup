bea3ab59fa3695299cb19a51c0857ecb5e51483d
Michaelmvv <MichaelMvv@users.noreply.github.com>
2017-08-02 12:56:13 -0700

    Working on routes and stuffs

---
+++ client/routes.js
@@ -0,0 +1,10 @@
+Router.route('/', function(){
+  this.layout('ApplicationLayout');
+  this.render('navBar', {to: 'top'});
+  this.render('landing')
+});
+Router.route('/dashboard',function(){
+  this.layout('ApplicationLayout');
+  this.render('navBar', {to: 'top'});
+  this.render('clientDashboard'); //logict to choose wich dashboard to render
+});
