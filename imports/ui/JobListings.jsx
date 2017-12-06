d7f87be5d59f799f508ea1053fe2d75e5daf55c6
Matthew Smith <Administrator@WTS7.attlocal.net>
2017-12-05 16:31:19 -0800

    Bootstrapify some stuff

--- imports/ui/JobListings.jsx
+++ imports/ui/JobListings.jsx
@@ -3,133 +3,101 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import classnames from 'classnames';
 import AppNavbar from './AppNavbar';
-import { Col, Panel, Grid, Row, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Col, Panel, Grid, Row, ListGroup, ListGroupItem, InputGroup, Label, Button } from 'react-bootstrap';
 
 export default class JobListings extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      languages: ["Java", "Python", "C", "C++", "C#", "F#", "VB", "JavaScript", "HTML", "TypeScript", "Rust", "PHP", "ASM", "Fortran", "Chef", "Perl" ]
+    };
+  }
+
   render() {
+    const languageList = this.state.languages.map((value) => {
+      return (
+        <div key={value} style={{fontSize: 16 + "px"}}>
+          <input type="checkbox" aria-label="..."/>&nbsp;{value}
+        </div>
+      );
+    });
+
     return(
     <Grid>
-        <div className="row">
-    </div>
     <Col md={4}>
-      <div align="center">
+      <div style={{margin: "auto"}}>
         <h2> Filters </h2>
       </div>
       <Panel>
-          <legend> Languages </legend>
-          <div style={{fontSize: 16 + "px"}}> <input type="checkbox" aria-label="..."/>  Java </div>
-          <div style={{fontSize: 16 + "px"}}> <input type="checkbox" aria-label="..."/>  Python </div>
-          <div style={{fontSize: 16 + "px"}}> <input type="checkbox" aria-label="..."/>  HTML </div>
-          <div style={{fontSize: 16 + "px"}}> <input type="checkbox" aria-label="..."/>  C </div>
-          <div style={{fontSize: 16 + "px"}}> <input type="checkbox" aria-label="..."/>  C++ </div>
-          <div style={{fontSize: 16 + "px"}}> <input type="checkbox" aria-label="..."/>  Rust </div>
+          <fieldset>
+            <legend> Languages </legend>
+            {languageList}
+          </fieldset>
           <br/>
-          <legend> Level Range </legend>
-          <div className="col-md-6">
-          <div className="input-group">
-            <span className="input-group-addon" id="basic-addon1">Low</span>
-            <input type="text" className="form-control" placeholder="0" aria-describedby="basic-addon1"/>
-          </div>
-        </div>
-        <div className="col-md-6">
-        <div className="input-group">
-          <span className="input-group-addon" id="basic-addon1">High</span>
-          <input type="text" className="form-control" placeholder="9" aria-describedby="basic-addon1"/>
-        </div>
-      </div>
-      <br/>
-      <br/>
-      <br/>
+          <fieldset>
+            <legend> Level Range </legend>
+            <Col md={6}>
+              <InputGroup>
+                <InputGroup.Addon>Low</InputGroup.Addon>
+              </InputGroup>
+            </Col>
+            <Col md={6}>
+              <InputGroup>
+               <InputGroup.Addon>High</InputGroup.Addon>
+              </InputGroup>
+           </Col>
+           </fieldset>
+      <br />
+      <br />
+      <br />
       <legend> Other Tags </legend>
-      <div className="input-group">
-        <span className="input-group-addon" id="basic-addon1">Tags</span>
-        <input type="text" className="form-control" placeholder="" aria-describedby="basic-addon1"/>
-      </div>
+      <InputGroup>
+        <InputGroup.Addon>Tags</InputGroup.Addon>
+      </InputGroup>
       </Panel>
     </Col>
-    <div className="col-md-8">
-      <div align="center">
+    <Col md={8}>
+      <div style={{margin: "auto"}}>
         <h2> Job Listings </h2>
       </div>
       <Panel>
           <h3> Job Title </h3>
-          <div className="currentTextDisabledSmall"> Posted By <a href="#"> Joe </a> </div>
-          <p> Lorem ipsum dolor sit amet, dicunt discere argumentum cum ei. Sint primis dicunt quo et, eam eu viris scripta accusam. Duo doming possim laboramus at. Eu commodo accusam scripserit nec, vel eu quidam audire, quo meis errem ea.
-          </p>
+          <div className="currentTextDisabledSmall"> Posted By <a href="#"> James Gosling </a> </div>
+          <p>I need someone to make me a Hello World program. I can't figure out how to use this language.</p>
           <br/>
-          <span className="label label-primary">Lv. 2</span>
-          <span className="label label-default">Java</span>
-          <span className="label label-default">For Loops</span>
+          <Label bsStyle="primary">Lv. 1</Label>
+          <Label bsStyle="default">Chef</Label>
+          <Label bsStyle="default">Hello World</Label>
           <div className="pull-right">
-            <button type="button" className="btn btn-success">View Job</button>
+            <Button bsStyle="success">View Job</Button>
           </div>
       </Panel>
       <Panel>
           <h3> Job Title </h3>
-          <div className="currentTextDisabledSmall"> Posted By <a href="#"> Joe </a> </div>
-          <p> Lorem ipsum dolor sit amet, dicunt discere argumentum cum ei. Sint primis dicunt quo et, eam eu viris scripta accusam. Duo doming possim laboramus at. Eu commodo accusam scripserit nec, vel eu quidam audire, quo meis errem ea.
-          </p>
+          <div className="currentTextDisabledSmall"> Posted By <a href="#"> Anonymous </a> </div>
+          <p>I need someone to make me a Turing-complete programming language. Syntax is irrelevant. I don't really care whether it's object oriented, functional, or whatever. I just need it.</p>
           <br/>
-          <span className="label label-primary">Lv. 2</span>
-          <span className="label label-default">Java</span>
-          <span className="label label-default">For Loops</span>
+          <Label bsStyle="primary">Lv. 6</Label>
+          <Label bsStyle="default">ASM</Label>
+          <Label bsStyle="default">Language Design</Label>
           <div className="pull-right">
-            <button type="button" className="btn btn-success">View Job</button>
+            <Button bsStyle="success">View Job</Button>
           </div>
       </Panel>
       <Panel>
           <h3> Job Title </h3>
-          <div className="currentTextDisabledSmall"> Posted By <a href="#"> Joe </a> </div>
-          <p> Lorem ipsum dolor sit amet, dicunt discere argumentum cum ei. Sint primis dicunt quo et, eam eu viris scripta accusam. Duo doming possim laboramus at. Eu commodo accusam scripserit nec, vel eu quidam audire, quo meis errem ea.
-          </p>
+          <div className="currentTextDisabledSmall"> Posted By <a href="#"> League Student </a> </div>
+          <p>I need someone to make me a Java spambot for educational purposes.</p>
           <br/>
-          <span className="label label-primary">Lv. 2</span>
-          <span className="label label-default">Java</span>
-          <span className="label label-default">For Loops</span>
+          <Label bsStyle="primary">Lv. 3</Label>
+          <Label bsStyle="default">Java</Label>
+          <Label bsStyle="default">Networking</Label>
           <div className="pull-right">
-            <button type="button" className="btn btn-success">View Job</button>
+            <Button bsStyle="success">View Job</Button>
           </div>
-        </Panel>
-      <Panel>
-          <h3> Job Title </h3>
-          <div className="currentTextDisabledSmall"> Posted By <a href="#"> Joe </a> </div>
-          <p> Lorem ipsum dolor sit amet, dicunt discere argumentum cum ei. Sint primis dicunt quo et, eam eu viris scripta accusam. Duo doming possim laboramus at. Eu commodo accusam scripserit nec, vel eu quidam audire, quo meis errem ea.
-          </p>
-          <br/>
-          <span className="label label-primary">Lv. 2</span>
-          <span className="label label-default">Java</span>
-          <span className="label label-default">For Loops</span>
-          <div className="pull-right">
-            <button type="button" className="btn btn-success">View Job</button>
-          </div>
-        </Panel>
-      <Panel>
-          <h3> Job Title </h3>
-          <div className="currentTextDisabledSmall"> Posted By <a href="#"> Joe </a> </div>
-          <p> Lorem ipsum dolor sit amet, dicunt discere argumentum cum ei. Sint primis dicunt quo et, eam eu viris scripta accusam. Duo doming possim laboramus at. Eu commodo accusam scripserit nec, vel eu quidam audire, quo meis errem ea.
-          </p>
-          <br/>
-          <span className="label label-primary">Lv. 2</span>
-          <span className="label label-default">Java</span>
-          <span className="label label-default">For Loops</span>
-          <div className="pull-right">
-            <button type="button" className="btn btn-success">View Job</button>
-          </div>
-        </Panel>
-      <Panel>
-          <h3> Job Title </h3>
-          <div className="currentTextDisabledSmall"> Posted By <a href="#"> Joe </a> </div>
-          <p> Lorem ipsum dolor sit amet, dicunt discere argumentum cum ei. Sint primis dicunt quo et, eam eu viris scripta accusam. Duo doming possim laboramus at. Eu commodo accusam scripserit nec, vel eu quidam audire, quo meis errem ea.
-          </p>
-          <br/>
-          <span className="label label-primary">Lv. 2</span>
-          <span className="label label-default">Java</span>
-          <span className="label label-default">For Loops</span>
-          <div className="pull-right">
-            <button type="button" className="btn btn-success">View Job</button>
-          </div>
-        </Panel>
-    </div>
+      </Panel>
+    </Col>
     </Grid>
     );
   }
