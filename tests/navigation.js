23bc89baab2d15b574de0d54bf947affe68d1eb7
Phil Cruz <phil@philcruz.com>
2018-01-16 16:51:48 -0800

    add testing framework

---
+++ tests/navigation.js
@@ -0,0 +1,16 @@
+describe("basic navigation", function() {
+	const pauseTime = 500;
+	const waitTime = 5000;
+	const waitTimeLong = 20000;
+
+	/*before(function() {
+		server.call("generateFixtures");
+	});*/
+
+	it("can try to go to the root", function() {
+		browser.url("http://localhost:3000");
+		const loginForm = browser.elements("form.accounts-ui");
+
+		assert.equal(browser.getText("div.page-header"), "Project GatewayX");
+	});
+});
