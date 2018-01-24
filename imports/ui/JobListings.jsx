a10601ddaef2c20970d614184a1d31252387cca5
Lucas Baizer <titaniumsapphire32@gmail.com>
2018-01-23 16:44:34 -0800

    Did some stuff to the jobs page & updated NPM

--- imports/ui/JobListings.jsx
+++ imports/ui/JobListings.jsx
@@ -11,7 +11,7 @@ import TrackerReact from 'meteor/ultimatejs:tracker-react';
 // Get the Collection
 Jobs = new Mongo.Collection("jobs");
 
-  function FieldGroup({ id, label, help, ...props }) {
+function FieldGroup({ id, label, help, ...props }) {
   return (
     <FormGroup controlId={id}>
       <ControlLabel>{label}</ControlLabel>
@@ -23,19 +23,19 @@ Jobs = new Mongo.Collection("jobs");
 
 export default class JobListings extends TrackerReact(React.Component) {
 
-    constructor() {
+  constructor() {
     super();
     this.state = {
-          subscription: {
-            jobs: Meteor.subscribe('jobs')
-          },
-          languages: ["Java", "Python", "C", "C++", "C#", "F#", "VB", "JavaScript", "HTML", "TypeScript", "Rust", "PHP", "ASM", "Fortran", "Chef", "Perl" ],
-          selectedLangs : [""],
-          lowerLevelFilter : 0,
-          upperLevelFilter : 9
-        };
-        this.langSelected = this.langSelected.bind(this);
-        this.levelRangeChanged = this.levelRangeChanged.bind(this);
+      subscription: {
+        jobs: Meteor.subscribe('jobs')
+      },
+      languages: ["Java", "Python", "C", "C++", "C#", "F#", "VB", "JavaScript", "HTML", "TypeScript", "Rust", "PHP", "ASM", "Fortran", "Chef", "Perl" ],
+      selectedLangs : [],
+      lowerLevelFilter : 0,
+      upperLevelFilter : 9
+    };
+    this.langSelected = this.langSelected.bind(this);
+    this.levelRangeChanged = this.levelRangeChanged.bind(this);
   }
 
   levelRangeChanged(ev){
@@ -49,7 +49,7 @@ export default class JobListings extends TrackerReact(React.Component) {
 
   langSelected(ev){
     if(ev.target.checked){
-     this.setState({ selectedLangs : [...this.state.selectedLangs, ev.target.dataset.lang]});
+      this.setState({ selectedLangs : [...this.state.selectedLangs, ev.target.dataset.lang]});
     }else{
       var indexToRemove = -1;
       for(i = 0; i < this.state.selectedLangs.length; i++){
@@ -59,7 +59,7 @@ export default class JobListings extends TrackerReact(React.Component) {
         }
       }
 
-      if(!(indexToRemove == -1)){
+      if(indexToRemove != -1){
         var newArray = this.state.selectedLangs;
         newArray.splice(indexToRemove, 1);
         this.setState({ selectedLangs : newArray});
@@ -76,13 +76,13 @@ export default class JobListings extends TrackerReact(React.Component) {
   }
 
   render() {
-  const languageList = this.state.languages.map((value) => {
+    const languageList = this.state.languages.map((value) => {
       return (
         <div key={value} style={{fontSize: 16 + "px"}}>
           <input type="checkbox" onChange={this.langSelected} data-lang={value} aria-label="..."/>&nbsp;{value}
         </div>
       );
-       });
+    });
     return(
       <div>
         <Grid>
@@ -123,43 +123,56 @@ export default class JobListings extends TrackerReact(React.Component) {
             </div>
             {
               this.jobs().map((job) => {
-              var shouldReturn = false;
-              for(i in job.langs){
-                for(j in this.state.selectedLangs){
-                  if(job.langs[i] === this.state.selectedLangs[j]){
-                    if(job.level >= this.state.lowerLevelFilter && job.level <= this.state.upperLevelFilter){
-                      shouldReturn = true;
+                var validLangs = false;
+                var validLevel = false;
+
+
+                if(this.state.selectedLangs.length > 0) {
+                  for(i in job.langs){
+                    for(j in this.state.selectedLangs){
+                      if(job.langs[i] === this.state.selectedLangs[j]){
+                        validLangs = true;
+                      }
                     }
                   }
+                } else {
+                  validLangs = true;
                 }
-              }
 
-              if(shouldReturn){
-                const langLabels = job.langs.map((lang) => {
-                  return(<span style={{"padding-right": "5px"}} key={job.toString() + lang.toString()}><Label bsStyle="default">{lang}</Label></span>);
-                });
-                return (
-                  <div key={job.name + job.desc + job.client + job.level + Math.random()}>
-                    <Panel>
-                      <h3> {job.name} </h3>
-                      <div className="currentTextDisabledSmall"> Posted By <a href="#"> {job.client} </a> </div>
-                      <p> {job.desc} </p>
-                      <br/>
-                      <span style={{"padding-right": "5px"}}><Label bsStyle="primary">Lv. {job.level}</Label></span>
-
-                      {langLabels}
-
-                      <div className="pull-right">
-                        <Button bsStyle="success">View Job</Button>
-                      </div>
-                    </Panel>
-                  </div>);
+                if(!(this.state.lowerLevelFilter == 0 && this.state.upperLevelFilter == 9)) {
+                  if(job.level >= this.state.lowerLevelFilter && job.level <= this.state.upperLevelFilter){
+                    validLevel = true;
+                  }
+                } else {
+                  validLevel = true;
                 }
-              })}
 
-            </Col>
-          </Grid>
-        </div>
-    );
-  }
-}
+                if(validLangs && validLevel){
+                  const langLabels = job.langs.map((lang) => {
+                    return(<span style={{"paddingRight": "5px"}} key={job.toString() + lang.toString()}><Label bsStyle="default">{lang}</Label></span>);
+                  });
+                  return (
+                    <div key={job.name + job.desc + job.client + job.level + Math.random()}>
+                      <Panel>
+                        <h3> {job.name} </h3>
+                        <div className="currentTextDisabledSmall"> Posted By <a href="#"> {job.client} </a> </div>
+                        <p> {job.desc} </p>
+                        <br/>
+                        <span style={{"paddingRight": "5px"}}><Label bsStyle="primary">Lv. {job.level}</Label></span>
+
+                        {langLabels}
+
+                        <div className="pull-right">
+                          <Button bsStyle="success">View Job</Button>
+                        </div>
+                      </Panel>
+                    </div>);
+                  }
+                })}
+
+              </Col>
+            </Grid>
+          </div>
+        );
+      }
+    }
