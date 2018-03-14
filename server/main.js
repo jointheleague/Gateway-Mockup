6c0fcca4a564f49a1f657062d4669738666d8603
Lucas Baizer <titaniumsapphire32@gmail.com>
2018-03-13 17:01:18 -0700

    Added more configuration stuff

--- server/main.js
+++ server/main.js
@@ -8,6 +8,7 @@ import { App } from '../imports/ui/App.jsx';
 const Jobs = new Mongo.Collection('jobs');
 const Languages = new Mongo.Collection('languages');
 const Profiles = new Mongo.Collection('profiles');
+const Messages = new Mongo.Collection('messages');
 
 Meteor.publish('jobs', function jobsPublication() {
   return Jobs.find();
@@ -16,6 +17,8 @@ Meteor.publish('jobs', function jobsPublication() {
 Meteor.startup(() => {
   pushSampleData(Jobs, Meteor.settings.sampledata.Jobs);
   pushSampleData(Languages, Meteor.settings.sampledata.Languages);
+  pushSampleData(Profiles, Meteor.settings.sampledata.Profiles);
+  pushSampleData(Messages, Meteor.settings.sampledata.Messages);
 });
 
 function pushSampleData(collection, data) {
