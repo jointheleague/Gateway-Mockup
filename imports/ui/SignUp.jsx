a9f517d0b714af9a8cf252c63d3855fa46da4a1d
Lucas Baizer <titaniumsapphire32@gmail.com>
2018-06-26 17:04:57 -0700

    Almost done, just needs redirect from onLogin hook

--- imports/ui/SignUp.jsx
+++ imports/ui/SignUp.jsx
@@ -10,74 +10,11 @@ export default class SignUp extends Component {
 
   constructor(){
     super();
-    this.handleUsername = this.handleUsername.bind(this);
-    this.handleEmail1 = this.handleEmail1.bind(this);
-    this.handleEmail2 = this.handleEmail2.bind(this);
-    this.handlePassword1 = this.handlePassword1.bind(this);
-    this.handlePassword2 = this.handlePassword2.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {
-      username: '',
-      usernamePlaceholder: 'Pick a username',
-      email1: '',
-      email1Placeholder: 'you@example.com',
-      email2: '',
-      email2Placeholder: 'Reenter email',
-      password1: '',
-      password1Placeholder: 'Create a password',
-      password2: '',
-      password2Placeholder: 'Reenter password',
-      validationState1: null,
-      validationState2: null,
-      validationState3: null
-    };
+    this.onLogin = this.onLogin.bind(this);
   }
 
-  handleUsername(e){
-    this.setState({ username: e.target.value});
-  }
-
-  handleEmail1(e){
-    this.setState({ email1: e.target.value});
-  }
-
-  handleEmail2(e){
-    this.setState({ email2: e.target.value});
-  }
-
-  handlePassword1(e){
-    this.setState({ password1: e.target.value});
-  }
-
-  handlePassword2(e){
-    this.setState({ password2: e.target.value});
-  }
-
-  handleSubmit(e){
-    if (this.state.username == '') this.setState({
-      usernamePlaceholder : 'You must enter a username.',
-      validationState1: 'error'});
-    else this.setState({validationState1: 'success'});
-    if (this.state.email1 == '') this.setState({
-      email1Placeholder : 'You must enter an email.',
-      validationState2: 'error'});
-    else if (this.state.email1 != this.state.email2) this.setState({
-      email1: '',
-      email2: '',
-      email1Placeholder : 'The emails do not match.',
-      email2Placeholder : 'The emails do not match.',
-      validationState2: 'error'});
-    else this.setState({validationState2: 'success'});
-    if (this.state.password1 == '') this.setState({
-      password1Placeholder : 'You must enter a password.',
-      validationState3: 'error'});
-    else if (this.state.password1 != this.state.password2) this.setState({
-      password1: '',
-      password2: '',
-      password1Placeholder : 'The passwords do not match.',
-      password2Placeholder : 'The passwords do not match.',
-      validationState3: 'error'});
-    else this.setState({validationState3: 'success'});
+  onLogin(){
+    Meteor.loginWithGithub();
   }
 
   render(){
@@ -94,7 +31,7 @@ export default class SignUp extends Component {
             </Row>
             <br />
           <Row>
-            <Accounts.ui.LoginForm />
+            <Button onClick={this.onLogin}> Login With GitHub </Button>
               </Row>
             </Grid>
           </div>
